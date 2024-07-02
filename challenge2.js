d998261681f4028288b5806bb78c13efcf2d700c
MichaelNgure <mikynjoro01@gmail.com>
2024-07-02 09:30:09 +0300

    modified challenge2

--- challenge2.js
+++ challenge2.js
@@ -5,13 +5,14 @@ const prompt = ps({sigint: true});
 const speed = prompt(("What is your speed? "));
 
 function speedOperator() {
+  parseInt(speed, 10)
   if (speed < 70) {
     return "OK";
   }
   if (speed >= 70) {
     let greatSpeed = speed - 70;
     let rem = greatSpeed / 5;
-    if (rem < 12) {
+    if (rem <= 12) {
       return `Points: ${rem}`;
     } else {
       return `License suspended`;
